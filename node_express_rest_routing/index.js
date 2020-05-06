e4e498326df77c229e05c2788450a28ce3b18286
piyush-cosmo <53268607+piyush-cosmo@users.noreply.github.com>
2020-05-07 04:04:14 +0530

    updated node_express_rest_routing

--- node_express_rest_routing/index.js
+++ node_express_rest_routing/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./dishRouter');
+const promoRouter = require('./promoRouter');
+const leaderRouter = require('./leaderRouter.js');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -11,8 +13,10 @@ const port = 3000;
 const app = express();
 
 app.use(morgan('dev'));
-app.use(express.static(__dirname + '/public'));
+
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 const server = http.createServer(app);
 
